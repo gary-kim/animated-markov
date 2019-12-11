56e85013888373e361ae484d614d053c573071d1
Gary Kim <gary@garykim.dev>
2019-12-11 08:44:36 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import * as math from 'mathjs';
+
+console.log(math.square([[2,0], [0,1]]));
+
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    entry: {
+        "index": path.join(__dirname, 'index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+    resolve: {
+        extensions: ['.js', '.vue'],
+    },
+    mode: "production",
+}
